6f39aa6d6e650a38712506477e8cc72df16d5e5f
kleyson castro <kleysoncastro99@gmail.com>
2020-07-29 00:21:17 -0400

    feat: completed put, post and delete routes 	repository registration, information update and project likes, node!

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +10,54 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories)
+  
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const {title, url, techs} = request.body;
+  const repo = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0
+  }
+  repositories.push(repo)
+  return response.send(repo);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params;
+  const {title, url, techs} =  request.body;
+
+  const repoIndex = repositories.findIndex(elemet => elemet.id === id);
+  if(repoIndex < 0) return response.status(400).json({erro: "Id not fund"})
+  const {likes} = repositories[repoIndex]
+
+  const repoFindUpdate = {
+    id, title, url, techs, likes
+  }
+  
+  repositories[repoIndex] = repoFindUpdate;
+  return response.send();
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params;
+  const repoIndex = repositories.findIndex(elemet => elemet.id === id);
+  if(repoIndex < 0) return response.status(400).json({erro: "Id not fund"})
+  repositories.splice(repoIndex, 1);
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const {id} = request.params;
+  const repoIndex = repositories.findIndex(elemet => elemet.id === id);
+  if(repoIndex < 0) return response.status(400).json({erro: "Id not fund"})
+
+  repositories[repoIndex].likes ++;
+  return response.json(repositories[repoIndex]);
 });
 
 module.exports = app;
